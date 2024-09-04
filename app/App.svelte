9cf2e4210e1da15be3ab1d7d7fc378b65f44c538
CLDXiang <417198670@qq.com>
2024-09-04 11:47:09 +0800

    reproduce

---
+++ app/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  function ongreeting(event: CustomEvent) {
+    console.log(event.detail);
+  }
+</script>
+
+<main>
+  <my-element {ongreeting}></my-element>
+</main>
---
+++ src/MyElement.svelte
@@ -0,0 +1,21 @@
+<svelte:options customElement="my-element" />
+
+<script lang="ts">
+	function ok(greeting: string) {
+		$host().dispatchEvent(
+			new CustomEvent('greeting', { detail: greeting })
+		);
+	}
+
+  function fail(greeting: string) {
+    const element = $host() as HTMLElement;
+		element.dispatchEvent(
+			new CustomEvent('greeting', { detail: greeting })
+		);
+	}
+</script>
+
+<div>
+  <button onclick={() => ok('hello')}>This will be ok</button>
+  <button onclick={() => fail('hello')}>This will throw</button>
+</div>
